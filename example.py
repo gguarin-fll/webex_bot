6b3ab739a94e21e663c1d641af9224ea1729ee35
Gio <gguarin@akamai.com>
2023-03-30 12:45:17 +0000

    Update example.py

--- example.py
+++ example.py
@@ -6,9 +6,10 @@ from webex_bot.webex_bot import WebexBot
 
 # Create a Bot Object
 bot = WebexBot(teams_bot_token="MjhhZjM3YzYtMjFmYS00NTFjLTlkODEtODg2NjVjZDFiNDgwNzBjNDg2YTgtNjY0_PF84_a3749315-ae09-4a52-806c-2c3222fa7c2c",
-               approved_rooms=['151a31b0-9895-11ec-baa3-df6b8f84527f'],
+               #approved_rooms=['151a31b0-9895-11ec-baa3-df6b8f84527f'],
                bot_name="Mimir",
-               include_demo_commands=True)
+               include_demo_commands=True,
+               approved_domains=["akamai.com"])
 
 # Add new commands for the bot to listen out for.
 bot.add_command(EchoCommand())
